57dcd648ae209cfa6bb150e75105fffd2bde8840
Yana <yana-kim-2002@mail.ru>
2024-09-22 22:42:51 +0300

    .

--- src/components/TicTacToe/components/Board/Board.jsx
+++ src/components/TicTacToe/components/Board/Board.jsx
@@ -14,15 +14,6 @@ function Board({ tiles, handleClick, player, strikeClass }) {
           player={player}
         />
       ))}
-      {/* <Tile value={tiles[0]} onClick={() => handleClick(0)} player={player} />
-      <Tile value={tiles[1]} onClick={() => handleClick(1)} player={player} />
-      <Tile value={tiles[2]} onClick={() => handleClick(2)} player={player} />
-      <Tile value={tiles[3]} onClick={() => handleClick(3)} player={player} />
-      <Tile value={tiles[4]} onClick={() => handleClick(4)} player={player} />
-      <Tile value={tiles[5]} onClick={() => handleClick(5)} player={player} />
-      <Tile value={tiles[6]} onClick={() => handleClick(6)} player={player} />
-      <Tile value={tiles[7]} onClick={() => handleClick(7)} player={player} />
-      <Tile value={tiles[8]} onClick={() => handleClick(8)} player={player} /> */}
       <Strike strikeClass={strikeClass} />
     </div>
   );
